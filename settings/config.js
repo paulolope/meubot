d05f6b9c65e1ed337817dbba6d04e4dc28612c97
paulolope <paulolope91@gmail.com>
2021-04-30 23:51:57 -0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'BigBraim',
+        ownerName: 'BOT JCKER',
+        youtube: '',
+        instagram: 'INSTAGRAM_LINK',
+}
---
+++ src/brayanmenu.js
@@ -0,0 +1,12 @@
+const adminmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Brayan*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.brayanmenu = adminmenu
